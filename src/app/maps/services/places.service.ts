1fbe77a1ed05cb7fc400ed78dcd501219c0bcae8
INDRA\mjhernando <mjhernando@minsait.com>
2024-10-27 20:37:33 +0100

    Creo un cliente http propio para facilitar la reutilizacion y sacar hardcode

--- src/app/maps/services/places.service.ts
+++ src/app/maps/services/places.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Feature, PlacesResponse } from '../interfaces/places';
+import { PlacesApiClient } from '../api/PlacesApiClient';
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +17,7 @@ export class PlacesService {
     return !!this.userLocation;
   }
 
-  constructor( private httpClient : HttpClient) { 
+  constructor( private placesApiClient : PlacesApiClient) { 
     this.getUserLocation();
   }
 
@@ -40,14 +41,26 @@ export class PlacesService {
   getPlacesByQuery(query: string = '') {
     // todo: evaluar cuando el query es nulo
 
+    if(!this.userLocation) throw Error('No hay user location');
+
     this.isLoadingPlaces = true;
 
-    this.httpClient.get<PlacesResponse>(`https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?country=es&limit=3&proximity=2.1445440374035343%2C41.41189955221725&language=es&access_token=pk.eyJ1IjoibWFnZGllbG1zIiwiYSI6ImNreXB6ZXJ1azBjeXEybnMyYm01bHk0aHAifQ.8iVI9ERFXi5ET09QbKJcww`)
-    .subscribe(resp => {
-      console.log(resp.features)
-      this.places = resp.features;
-      this.isLoadingPlaces = false;
-    });
+    // this.httpClient.get<PlacesResponse>(`${query}.json?country=es&limit=3&proximity=2.1445440374035343%2C41.41189955221725&language=es&access_token=pk.eyJ1IjoibWFnZGllbG1zIiwiYSI6ImNreXB6ZXJ1azBjeXEybnMyYm01bHk0aHAifQ.8iVI9ERFXi5ET09QbKJcww`)
+    // .subscribe(resp => {
+    //   console.log(resp.features)
+    //   this.places = resp.features;
+    //   this.isLoadingPlaces = false;
+    // });
 
+    this.placesApiClient.get<PlacesResponse>(`/${query}.json`, {
+      params: {
+        proximity: this.userLocation.join(',')
+      }
+    })
+    .subscribe(resp => {
+        console.log(resp.features)
+        this.places = resp.features;
+        this.isLoadingPlaces = false;
+      });
   }
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,3 @@
+export const environment = {
+    apiKey: 'pk.eyJ1IjoibWFnZGllbG1zIiwiYSI6ImNreXB6ZXJ1azBjeXEybnMyYm01bHk0aHAifQ.8iVI9ERFXi5ET09QbKJcww'
+};
